192561e0052245f56f6baa75c1af9da7c91472e8
KonkreetBaybee <ethan.poitras1@gmail.com>
2025-08-09 01:37:10 -0600

    Update lineage_generic.mk

--- lineage_generic.mk
+++ lineage_generic.mk
@@ -5,6 +5,11 @@
 #
 
 # Inherit from those products. Most specific first.
+
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from lynx device
+$(call inherit-product, device/google/lynx/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
